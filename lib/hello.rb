d7925e1660010f9889d3db6d802a8b9732f67965
kyle Dornan <kyledornan26@gmail.com>
2020-06-01 12:02:28 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,15 +1,4 @@
-def hello_t(array)
-    i = 0
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-   end 
-   array
-   
-    puts "Hey! No block was given!"
-  end
-  array
-end
+
 
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
